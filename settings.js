d0dc72c65bf47410b8fe398a35981a0b46d6343c
christian773 <113441518+christian773@users.noreply.github.com>
2022-09-28 10:47:22 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,14 +19,14 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+2349044102961']
+global.premium = ['+2349044102961']
+global.ownernomer = '+2349044102961'
+global.ownername = '🐦ChrisTian'
+global.botname = 'Chris-Bot'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.region = 'Nigeria, South lagos, ikeja'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
 global.packname = 'Gojo-Satoru'
